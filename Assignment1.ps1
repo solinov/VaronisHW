2629f0d5ffae71837d455207eb789395a27f2c5a
Stas Olinov <solinov@gmail.com>
2024-01-22 18:01:28 +0200

    added new code

--- Assignment1.ps1
+++ Assignment1.ps1
@@ -20,3 +20,243 @@ terraform apply -target module.install_ad --auto-approve
 terraform apply -target module.group_user_creation --auto-approve
 
 # Pause
+
+
+new code
+
+new code 2
+cd D:\VaronisHW # change to project path
+
+terraform init
+
+# create requested resources
+terraform apply -target azurerm_resource_group.SOVar_RG --auto-approve
+terraform apply -target module.network --auto-approve
+terraform apply -target module.dc --auto-approve
+terraform apply -target module.server --auto-approve
+
+# install AD on dc
+terraform apply -target module.install_ad --auto-approve
+
+# missing logic to verify domain was created before joining server to the newly created domain
+
+# join server to domain
+#terraform apply -target azurerm_virtual_machine_extension.server_join_domain --auto-approve
+
+# AD group and user creation
+terraform apply -target module.group_user_creation --auto-approve
+
+# Pause
+
+
+new code
+
+new code 2cd D:\VaronisHW # change to project path
+
+terraform init
+
+# create requested resources
+terraform apply -target azurerm_resource_group.SOVar_RG --auto-approve
+terraform apply -target module.network --auto-approve
+terraform apply -target module.dc --auto-approve
+terraform apply -target module.server --auto-approve
+
+# install AD on dc
+terraform apply -target module.install_ad --auto-approve
+
+# missing logic to verify domain was created before joining server to the newly created domain
+
+# join server to domain
+#terraform apply -target azurerm_virtual_machine_extension.server_join_domain --auto-approve
+
+# AD group and user creation
+terraform apply -target module.group_user_creation --auto-approve
+
+# Pause
+
+
+new code
+
+new code 2cd D:\VaronisHW # change to project path
+
+terraform init
+
+# create requested resources
+terraform apply -target azurerm_resource_group.SOVar_RG --auto-approve
+terraform apply -target module.network --auto-approve
+terraform apply -target module.dc --auto-approve
+terraform apply -target module.server --auto-approve
+
+# install AD on dc
+terraform apply -target module.install_ad --auto-approve
+
+# missing logic to verify domain was created before joining server to the newly created domain
+
+# join server to domain
+#terraform apply -target azurerm_virtual_machine_extension.server_join_domain --auto-approve
+
+# AD group and user creation
+terraform apply -target module.group_user_creation --auto-approve
+
+# Pause
+
+
+new code
+
+new code 2cd D:\VaronisHW # change to project path
+
+terraform init
+
+# create requested resources
+terraform apply -target azurerm_resource_group.SOVar_RG --auto-approve
+terraform apply -target module.network --auto-approve
+terraform apply -target module.dc --auto-approve
+terraform apply -target module.server --auto-approve
+
+# install AD on dc
+terraform apply -target module.install_ad --auto-approve
+
+# missing logic to verify domain was created before joining server to the newly created domain
+
+# join server to domain
+#terraform apply -target azurerm_virtual_machine_extension.server_join_domain --auto-approve
+
+# AD group and user creation
+terraform apply -target module.group_user_creation --auto-approve
+
+# Pause
+
+
+new code
+
+new code 2cd D:\VaronisHW # change to project path
+
+terraform init
+
+# create requested resources
+terraform apply -target azurerm_resource_group.SOVar_RG --auto-approve
+terraform apply -target module.network --auto-approve
+terraform apply -target module.dc --auto-approve
+terraform apply -target module.server --auto-approve
+
+# install AD on dc
+terraform apply -target module.install_ad --auto-approve
+
+# missing logic to verify domain was created before joining server to the newly created domain
+
+# join server to domain
+#terraform apply -target azurerm_virtual_machine_extension.server_join_domain --auto-approve
+
+# AD group and user creation
+terraform apply -target module.group_user_creation --auto-approve
+
+# Pause
+
+
+new code
+
+new code 2cd D:\VaronisHW # change to project path
+
+terraform init
+
+# create requested resources
+terraform apply -target azurerm_resource_group.SOVar_RG --auto-approve
+terraform apply -target module.network --auto-approve
+terraform apply -target module.dc --auto-approve
+terraform apply -target module.server --auto-approve
+
+# install AD on dc
+terraform apply -target module.install_ad --auto-approve
+
+# missing logic to verify domain was created before joining server to the newly created domain
+
+# join server to domain
+#terraform apply -target azurerm_virtual_machine_extension.server_join_domain --auto-approve
+
+# AD group and user creation
+terraform apply -target module.group_user_creation --auto-approve
+
+# Pause
+
+
+new code
+
+new code 2cd D:\VaronisHW # change to project path
+
+terraform init
+
+# create requested resources
+terraform apply -target azurerm_resource_group.SOVar_RG --auto-approve
+terraform apply -target module.network --auto-approve
+terraform apply -target module.dc --auto-approve
+terraform apply -target module.server --auto-approve
+
+# install AD on dc
+terraform apply -target module.install_ad --auto-approve
+
+# missing logic to verify domain was created before joining server to the newly created domain
+
+# join server to domain
+#terraform apply -target azurerm_virtual_machine_extension.server_join_domain --auto-approve
+
+# AD group and user creation
+terraform apply -target module.group_user_creation --auto-approve
+
+# Pause
+
+
+new code
+
+new code 2cd D:\VaronisHW # change to project path
+
+terraform init
+
+# create requested resources
+terraform apply -target azurerm_resource_group.SOVar_RG --auto-approve
+terraform apply -target module.network --auto-approve
+terraform apply -target module.dc --auto-approve
+terraform apply -target module.server --auto-approve
+
+# install AD on dc
+terraform apply -target module.install_ad --auto-approve
+
+# missing logic to verify domain was created before joining server to the newly created domain
+
+# join server to domain
+#terraform apply -target azurerm_virtual_machine_extension.server_join_domain --auto-approve
+
+# AD group and user creation
+terraform apply -target module.group_user_creation --auto-approve
+
+# Pause
+
+
+new code
+
+new code 2cd D:\VaronisHW # change to project path
+
+terraform init
+
+# create requested resources
+terraform apply -target azurerm_resource_group.SOVar_RG --auto-approve
+terraform apply -target module.network --auto-approve
+terraform apply -target module.dc --auto-approve
+terraform apply -target module.server --auto-approve
+
+# install AD on dc
+terraform apply -target module.install_ad --auto-approve
+
+# missing logic to verify domain was created before joining server to the newly created domain
+
+# join server to domain
+#terraform apply -target azurerm_virtual_machine_extension.server_join_domain --auto-approve
+
+# AD group and user creation
+terraform apply -target module.group_user_creation --auto-approve
+
+# Pause
+
+
+new code
+
+new code 2
